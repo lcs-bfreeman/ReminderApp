26447e7cf634321937401b7206d59a4911ebc997
Benjamn <bfreeman@lcs.on.ca>
2022-01-20 12:20:00 -0500

    Added colours for each task priority

--- ReminderApp/Views/TaskCell.swift
+++ ReminderApp/Views/TaskCell.swift
@@ -11,6 +11,18 @@ struct TaskCell: View {
     
     @ObservedObject var task: Task
     
+    var taskColor: Color {
+        switch task.priority {
+        case .high:
+            return Color.red
+        case .medium:
+            return Color.blue
+        case .low:
+            return Color.primary
+            
+        }
+    }
+    
     var body: some View {
         
         HStack {
@@ -23,7 +35,7 @@ struct TaskCell: View {
             
             Text(task.description)
         }
-        
+        .foregroundColor(self.taskColor)
     }
 }
 
