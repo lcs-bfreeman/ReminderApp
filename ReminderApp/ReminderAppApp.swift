969869c07146f26985055369cf2f9bc3be34b79b
Benjamn <bfreeman@lcs.on.ca>
2022-01-20 11:18:23 -0500

    Initial Commit

---
+++ ReminderApp/ReminderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderAppApp.swift
+//  ReminderApp
+//
+//  Created by Ben Freeman on 2022-01-20.
+//
+
+import SwiftUI
+
+@main
+struct ReminderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
