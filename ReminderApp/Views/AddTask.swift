d93bb2d6ad6931858f95f843505c4f12cdfd1416
Benjamn <bfreeman@lcs.on.ca>
2022-01-25 08:57:48 -0500

    Added a cancel button to the add task sheet

--- ReminderApp/Views/AddTask.swift
+++ ReminderApp/Views/AddTask.swift
@@ -38,6 +38,12 @@ struct AddTask: View {
                         saveTask()
                     }
                 }
+                
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel") {
+                        showing = false
+                    }
+                }
             }
         }
     }
